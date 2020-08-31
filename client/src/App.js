d8be2c22e493f0f25b56f2b2520ac6d7bb5f6b2c
unknown <ifzalkola@gmail.com>
2020-08-31 15:58:58 +0530

    restructured the project structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [file, setFile] = useState({ file: null });
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target.files[0]);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" onSubmit={handleSubmit}>
+      <form>
+        <label htmlFor="text-file">Select a text file</label>
+        <input type="file" id="text-file"></input>
+        <input type="submit"></input>
+      </form>
     </div>
   );
-}
+};
 
 export default App;
